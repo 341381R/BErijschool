9c2399a7aa25229b1667ad4421b2973feb5bed49
unknown <341381@studet.mboutrecht.nl>
2026-05-24 13:00:06 +0200

    de functie voor de model van de index pagina gemaakt

--- app/Models/InstructeurModel.php
+++ app/Models/InstructeurModel.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class InstructeurModel extends Model
 {
-    //
+    public function SP_GetAllInstructeurs()
+    {
+        return DB::select('CALL SP_GetAllInstructeurs');
+    }
 }
