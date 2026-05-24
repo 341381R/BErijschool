a3787f15c6e3aeda05433424a2c542ecb43c1efa
unknown <341381@studet.mboutrecht.nl>
2026-05-24 14:18:48 +0200

    de stored procedure aangemaakt voor de view

---
+++ database/createscript/SP_GetAllInstructeurs.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS SP_GetAllInstructeurs;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_GetAllInstructeurs()
+BEGIN
+
+    SELECT   ISTR.Id
+            ,(CONCAT_WS(" ", ISTR.Voornaam, ISTR.Tussenvoegsel, ISTR.Achternaam)) AS Naam
+            ,ISTR.Mobiel
+            ,ISTR.DatumInDienst
+            ,LENGTH(ISTR.AantalSterren) AS AantalSterren
+    FROM Instructeur AS ISTR
+    ORDER BY LENGTH(ISTR.AantalSterren);
+
+
+END$$
+
+DELIMITER ;
