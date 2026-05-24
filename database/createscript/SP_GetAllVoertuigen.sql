dc8c5b3b796a7a4c0e4cc5c970ef557339c7e6a4
unknown <341381@studet.mboutrecht.nl>
2026-05-24 21:46:17 +0200

    nieuwe stored procedure aangemaakt voor het opvragen van alle data over voertuigen

---
+++ database/createscript/SP_GetAllVoertuigen.sql
@@ -0,0 +1,29 @@
+DROP PROCEDURE IF EXISTS SP_GetAllVoertuigen;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_GetAllVoertuigen()
+BEGIN
+
+    SELECT   ISTR.Id AS InstructeurId
+            ,VRTG.Id AS VoertuigId
+            ,(CONCAT_WS(" ", ISTR.Voornaam, ISTR.Tussenvoegsel, ISTR.Achternaam)) AS Naam
+            ,ISTR.DatumInDienst
+            ,TPVG.TypeVoertuig
+            ,VRTG.Type
+            ,VRTG.Kenteken
+            ,VRTG.Bouwjaar
+            ,VRTG.Brandstof
+            ,TPVG.Rijbewijscategorie
+    FROM Instructeur AS ISTR
+    INNER JOIN VoertuigInstructeur AS VGIR
+    ON ISTR.Id = VGIR.InstructeurId
+    INNER JOIN Voertuig AS VRTG
+    ON VRTG.Id = VGIR.VoertuigId
+    INNER JOIN TypeVoertuig AS TPVG
+    ON TPVG.Id = VRTG.TypeVoertuigId
+
+
+END$$
+
+DELIMITER ;
