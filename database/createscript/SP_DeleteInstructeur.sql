4b25051cdc9b4177f15bf7816ec80db02f7c68b2
unknown <341381@studet.mboutrecht.nl>
2026-06-07 14:35:55 +0200

    SP_DeleteInstructeur aangemaakt

---
+++ database/createscript/SP_DeleteInstructeur.sql
@@ -0,0 +1,22 @@
+DROP PROCEDURE IF EXISTS SP_DeleteInstructeur;
+
+DELIMITER $$
+
+CREATE PROCEDURE SP_DeleteInstructeur(
+    IN p_VoertuigId                 INT
+    IN p_VoertuigInstructeurId      INT
+)
+
+BEGIN
+    DELETE FROM VoertuigInstructeur AS VGIR
+        WHERE VGIR.Id = p_VoertuigInstructeurid;
+
+        SELECT ROW_COUNT() AS affected;
+
+    UPDATE Voertuig AS VRTG
+        SET     VRTG.IsActief = 0
+        WHERE   VRTG.Id = p_VoertuigId;
+
+END$$
+
+DELIMITER ;
